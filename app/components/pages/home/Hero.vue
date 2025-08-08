649ce424a644d9dfa04f5832f42825376bfa6d48
Valou0539 <valentin.gindre@gmail.com>
2025-08-08 12:32:52 +0200

    feat(home): rename heading by hero

--- app/components/pages/home/Hero.vue
+++ app/components/pages/home/Hero.vue
@@ -3,12 +3,12 @@
     class="pb-18 relative mx-auto mt-20 max-w-5xl px-4 text-center md:mt-28"
   >
     <h1 class="heading-1 mb-[clamp(32px,5vw,48px)]">
-      {{ $t("home.heading.title") }}
+      {{ $t("home.hero.title") }}
     </h1>
     <p
       class="sm:max-w-7/12 mx-auto w-[284px] max-w-full text-[clamp(16px,3vw,22px)] sm:w-auto"
     >
-      {{ $t("home.heading.subtitle") }}
+      {{ $t("home.hero.subtitle") }}
     </p>
     <assets-glass-code
       class="absolute -right-1 -top-1 size-[clamp(40px,12vw,112px)] sm:-top-4 sm:right-[clamp(40px,calc(80px-2vw),64px)]"
@@ -18,5 +18,3 @@
     />
   </section>
 </template>
-
-<script lang="ts" setup></script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-    <PagesHomeHeading />
+    <PagesHomeHero />
     <PagesHomeWhatIDo />
     <PagesHomeFeaturedProjects />
     <PagesHomeCTA />
