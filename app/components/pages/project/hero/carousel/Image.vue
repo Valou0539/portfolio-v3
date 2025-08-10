19915a8d2d917a03002d3737ed1f6aa4c27d49cd
Valou0539 <valentin.gindre@gmail.com>
2025-08-10 21:10:17 +0200

    feat(carousel): optimize images

--- app/components/pages/project/hero/carousel/Image.vue
+++ app/components/pages/project/hero/carousel/Image.vue
@@ -1,25 +1,6 @@
 <template>
-  <NuxtImg
-    :src="image?.src"
-    :alt="image?.alt[$i18n.locale]"
-    :width="1920"
-    :height="1080"
-    :class="[
-      'mr-2 size-full object-cover sm:rounded-lg md:rounded-xl lg:rounded-2xl',
-      {
-        'opacity-50':
-          (index !== 1 || state !== 'movingPrev') &&
-          (index !== 2 || state !== 'idle') &&
-          (index !== 3 || state !== 'movingNext') &&
-          multiple,
-      },
-      {
-        'transition-opacity duration-500 ease-in-out': state !== 'idle',
-      },
-    ]"
-    sizes="100vw sm:640px md:960px lg:1920px"
-    fetchpriority="high"
-  />
+  <NuxtImg v-bind="imageAttributes" class="md:hidden" :quality="50" />
+  <NuxtImg v-bind="imageAttributes" class="hidden lg:block" :quality="100" />
 </template>
 <script lang="ts" setup>
 import type { ProjectsCarouselCollectionItem } from "@nuxt/content";
@@ -30,5 +11,30 @@ interface Props {
   multiple?: boolean;
 }
 
-defineProps<Props>();
+const props = defineProps<Props>();
+
+const { locale } = useI18n();
+
+const imageAttributes = computed(() => {
+  return {
+    src: props.image.src,
+    alt: props.image.alt[locale.value],
+    width: 1920,
+    height: 1080,
+    class: [
+      "mr-2 size-full object-cover sm:rounded-lg md:rounded-xl lg:rounded-2xl",
+      {
+        "opacity-50":
+          (props.index !== 1 || props.state !== "movingPrev") &&
+          (props.index !== 2 || props.state !== "idle") &&
+          (props.index !== 3 || props.state !== "movingNext") &&
+          props.multiple,
+      },
+      {
+        "transition-opacity duration-500 ease-in-out": props.state !== "idle",
+      },
+    ],
+    fetchpriority: "high",
+  };
+});
 </script>
