594c59b0b015028f143e98f29e4d7bdec0f0f4ce
Valou0539 <valentin.gindre@gmail.com>
2025-08-13 01:22:55 +0200

    fix(carousel): fix image size (on mobile only)

--- app/components/pages/project/hero/carousel/Image.vue
+++ app/components/pages/project/hero/carousel/Image.vue
@@ -1,7 +1,7 @@
 <template>
   <NuxtImg
     :src="props.image.src"
-    class="mr-2 size-full h-full w-full flex-grow object-cover transition-opacity duration-500 ease-in-out sm:rounded-lg md:rounded-xl lg:rounded-2xl"
+    class="mr-2 h-full flex-grow object-cover transition-opacity duration-500 ease-in-out sm:rounded-lg md:rounded-xl lg:rounded-2xl"
     :fetchpriority="props.fetchPriorityHigh ? 'high' : undefined"
     :alt="props.image.alt[$i18n.locale]"
     width="1920"
