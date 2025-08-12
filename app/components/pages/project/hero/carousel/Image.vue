5d0d6422a00ef84ffa0acb0eb62d031bcc162877
Valou0539 <valentin.gindre@gmail.com>
2025-08-13 01:28:39 +0200

    fix(carousel): fix image size (on mobile only)

--- app/components/pages/project/hero/carousel/Image.vue
+++ app/components/pages/project/hero/carousel/Image.vue
@@ -1,12 +1,16 @@
 <template>
-  <NuxtImg
-    :src="props.image.src"
-    class="mr-2 block h-full flex-grow object-cover transition-opacity duration-500 ease-in-out sm:rounded-lg md:rounded-xl lg:rounded-2xl"
-    :fetchpriority="props.fetchPriorityHigh ? 'high' : undefined"
-    :alt="props.image.alt[$i18n.locale]"
-    width="1920"
-    height="1080"
-  />
+  <div
+    class="mr-2 size-full flex-grow transition-opacity duration-500 ease-in-out"
+  >
+    <NuxtImg
+      :src="props.image.src"
+      class="size-full object-cover sm:rounded-lg md:rounded-xl lg:rounded-2xl"
+      :fetchpriority="props.fetchPriorityHigh ? 'high' : undefined"
+      :alt="props.image.alt[$i18n.locale]"
+      width="1920"
+      height="1080"
+    />
+  </div>
 </template>
 <script lang="ts" setup>
 import type { ProjectsCarouselCollectionItem } from "@nuxt/content";
