b194c56565a6448f0a75e52780c9a6e8204cc328
Valou0539 <valentin.gindre@gmail.com>
2025-08-15 00:55:38 +0200

    wip: implement project page

--- app/pages/projects/index.vue
+++ app/pages/projects/index.vue
@@ -1,5 +1,18 @@
 <template>
-  <main></main>
+  <main>
+    <section class="mx-auto mt-20 max-w-sm px-4 sm:max-w-5xl md:mt-28">
+      <h1 class="heading-1 mb-8 text-center md:mb-12">
+        {{ t("projects.title") }}
+      </h1>
+      <div class="grid grid-cols-1 gap-6 sm:grid-cols-3 md:gap-16">
+        <CommonProjectCard
+          v-for="project in projects"
+          :key="project.id"
+          :project="project"
+        />
+      </div>
+    </section>
+  </main>
 </template>
 <script lang="ts" setup>
 const { t } = useI18n();
@@ -8,4 +21,6 @@ useSeoMeta({
   title: t("projects.seo.title"),
   description: t("projects.seo.description"),
 });
+
+const { data: projects } = await useProjects();
 </script>
